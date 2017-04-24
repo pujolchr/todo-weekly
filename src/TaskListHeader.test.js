8c9b9140743caf276ac8b2cdff7b3b649dd3d5f4
Christian Svendsen Pujol <pujolchr@gmail.com>
2017-04-24 23:34:39 +0200

    file moved in __tests__

--- src/TaskListHeader.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import TaskListHeader from './TaskListHeader';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<TaskListHeader />, div);
-});
--- src/WeekDays.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import WeekDays from './WeekDays';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  const week = {};
-  ReactDOM.render(<WeekDays week={week} />, div);
-});
